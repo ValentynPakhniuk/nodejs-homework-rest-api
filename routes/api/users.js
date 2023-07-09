52ef35d07dc6af47581effa0a4a52c148d317cd8
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-07-09 16:55:41 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const { validation, auth, upload, ctrlWrapper } = require("../../middlewares");
 const { users: ctrl } = require("../../controllers");
-const { joiRegisterSchema, joiLoginSchema } = require("../../schemas/user");
+const {
+  joiRegisterSchema,
+  joiLoginSchema,
+  joiVerifySchema,
+} = require("../../schemas/user");
 
 const router = express.Router();
 
@@ -25,4 +29,41 @@ router.patch(
   ctrlWrapper(ctrl.updateAvatar)
 );
 
+router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail));
+
+router.post(
+  "/verify",
+  validation(joiVerifySchema, "missing required field email"),
+  ctrlWrapper(ctrl.reVerifyEmail)
+);
+
 module.exports = router;
+
+// // using Twilio SendGrid's v3 Node.js Library
+// // https://github.com/sendgrid/sendgrid-nodejs
+// javascript
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const msg = {
+//   to: 'test@example.com', // Change to your recipient
+//   from: 'test@example.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+
+// npm install --save @sendgrid/mail
+
+// echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+// echo "sendgrid.env" >> .gitignore
+// source ./sendgrid.env
+
+// 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;
