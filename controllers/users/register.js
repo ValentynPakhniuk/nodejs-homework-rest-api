d215afc06268cc70be05bd2b15164d006b3949fe
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-07-09 16:55:20 +0300

    Update register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,7 +1,9 @@
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
 
+const { sendEmail } = require("../../helpers");
 const { User } = require("../../models");
 
 const register = async (req, res) => {
@@ -11,6 +13,7 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict("Email in use");
   }
+  const verificationToken = v4();
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   const avatarURL = gravatar.url(email);
   const result = await User.create({
@@ -18,7 +21,16 @@ const register = async (req, res) => {
     password: hashPassword,
     subscription,
     avatarURL,
+    verificationToken,
   });
+  const mail = {
+    to: email,
+    subject: `Підтвердження email`,
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Підтвердити email</a>`,
+  };
+
+  await sendEmail(mail);
+
   res.status(201).json({
     status: "success",
     code: 201,
@@ -27,6 +39,7 @@ const register = async (req, res) => {
         email: result.email,
         subscription: result.subscription,
         avatarURL: result.avatarURL,
+        verificationToken: result.verificationToken,
       },
     },
   });
