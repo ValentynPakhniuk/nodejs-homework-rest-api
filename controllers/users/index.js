0777faef3ac69fbcc057bc12bc0e9188976f327d
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-07-09 16:55:38 +0300

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,5 +3,15 @@ const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const reVerifyEmail = require("./reVerifyEmail");
 
-module.exports = { register, login, logout, getCurrent, updateAvatar };
+module.exports = {
+  register,
+  login,
+  logout,
+  getCurrent,
+  updateAvatar,
+  verifyEmail,
+  reVerifyEmail,
+};
