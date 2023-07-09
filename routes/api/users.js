e99ebf445b7c0e35dbf0255cc634bf612112cc33
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-07-09 17:04:16 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,32 +38,3 @@ router.post(
 );
 
 module.exports = router;
-
-// // using Twilio SendGrid's v3 Node.js Library
-// // https://github.com/sendgrid/sendgrid-nodejs
-// javascript
-// const sgMail = require('@sendgrid/mail')
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-// const msg = {
-//   to: 'test@example.com', // Change to your recipient
-//   from: 'test@example.com', // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
-
-// npm install --save @sendgrid/mail
-
-// echo "export SENDGRID_API_KEY='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'" > sendgrid.env
-// echo "sendgrid.env" >> .gitignore
-// source ./sendgrid.env
-
-// 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;
